1ecd281a59457a05861de9b0d7b65638d9cc323a
Шахрезада Кожонова <unicorny.monster@gmail.com>
2021-09-15 17:03:19 +0600

    lineGraph added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import { FormControl, MenuItem, Select } from "@material-ui/core";
+import {
+  FormControl,
+  MenuItem,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
 import { useEffect, useState } from "react";
 // import InfoBox
 import "./App.css";
 import InfoBox from "./InfoBox";
+import Map from "./Map";
+import Table from "./Table";
+import { sortData } from "./util";
+import LineGraph from "./LineGraph";
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    //при первой загрузке показывает worldwide
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -17,6 +38,8 @@ function App() {
             name: country.country, //United States
             value: country.countryInfo.iso2, //UK, USA
           }));
+          const sortedData = sortData(data); //функция сортировки стран в таблице util.js
+          setTableData(sortedData);
           setCountries(countries);
         });
     };
@@ -24,36 +47,69 @@ function App() {
   }, []);
   const onCountryChange = async (event) => {
     const countryCode = event.target.value;
-    setCountry(countryCode);
+
+    const url =
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode);
+        //all of the data...
+        // from the country response
+        setCountryInfo(data);
+      });
   };
 
   return (
     <div className="app">
-      <div className="app__header">
-        <h1>COVID-19 TRACKER</h1>
-        <FormControl className="app_dropdown">
-          <Select variant="outlined" onChange={onCountryChange} value={country}>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
-            {/* <MenuItem value="worldwide"> Worldwide</MenuItem>
-          <MenuItem value="worldwide"> Option1</MenuItem>
-          <MenuItem value="worldwide"> Option2</MenuItem>
-          <MenuItem value="worldwide"> Option3</MenuItem> */}
-          </Select>
-        </FormControl>
-      </div>
-      <div className="app__stats">
-        <InfoBox title="Coronavirus Cases" />
-        <InfoBox title="Recovered" />
-        <InfoBox title="Deaths" />
-      </div>
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className="app_dropdown">
+            <Select
+              variant="outlined"
+              onChange={onCountryChange}
+              value={country}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+        <div className="app__stats">
+          <InfoBox
+            title="Coronavirus Cases"
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
+          />
+          <InfoBox
+            title="Recovered"
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
+          />
+          <InfoBox
+            title="Deaths"
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
+          />
+        </div>
 
-      {/* Table */}
-      {/* Graph */}
-
-      {/* Map */}
+        {/* Map */}
+        <Map />
+      </div>
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          <Table countries={tableData} />
+          <h3>Worldwide new cases</h3>
+          <LineGraph />
+          {/* Graph */}
+        </CardContent>
+      </Card>
     </div>
   );
 }
---
+++ src/Map.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Map() {
+  return (
+    <div className="map">
+      <h1>I am a Map</h1>
+    </div>
+  );
+}
